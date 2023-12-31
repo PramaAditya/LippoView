ff73025938c2dbbd8903c155bcfb8ec8d74f4aab
Developer <prama13@gmail.com>
2023-12-31 18:53:38 +0700

    Initial Files

---
+++ TableDataModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lippo.Models
+{
+    public class TableDataIndexModel
+    {
+        public string Category { get; set; }
+        public List<InputTableData> DataList { get; set; }
+    }
+
+    public class TableDataEditModel
+    {
+        public string ReturnUrl { get; set; }
+        public InputTableData Data { get; set; }
+    }
+
+    public class InputTableData
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Data")]
+        public string Description { get; set; }
+        [Required]
+        public string Category { get; set; }
+    }
+}
